544fb795926ebe449fedcf59515636a51aa337a3
Jose E. Gonzalez Modecir <jose.enrique.gonzalez.modecir@sap.com>
2018-10-19 15:24:47 +0200

    Push screen mocked

--- front-end/src/app/components/push/push.component.ts
+++ front-end/src/app/components/push/push.component.ts
@@ -1,8 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { timer } from 'rxjs';
 
 @Component({
   selector: 'push',
   templateUrl: './push.component.html'
 })
-export class PushComponent {
+export class PushComponent implements OnInit {
+
+    receivedData: Array<any>;
+
+    ngOnInit() {
+        this.receivedData = new Array<any>();
+
+        let timer0 = timer(0, 500);
+
+        timer0.subscribe(() => {
+            this.receivedData.push(this.newElement());
+        });
+    }
+
+    newElement():any {
+
+        var currentElement = {
+            id: Math.floor(Math.random() * 1e100),
+            dateTime: Date.now(),
+            value: Math.floor(Math.random() * 1000)
+        }
+
+        return currentElement;
+    }
+
+    clearReceivedData() {
+        this.receivedData = new Array<any>();
+    }
+
 }
