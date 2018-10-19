d52b8bf2de765adb1d5f58bde83d3d24afdcff8d
Jose E. Gonzalez Modecir <jose.enrique.gonzalez.modecir@sap.com>
2018-10-19 14:50:52 +0200

    Send Await screen mocked up

--- front-end/src/app/components/send-await/send-await.component.ts
+++ front-end/src/app/components/send-await/send-await.component.ts
@@ -10,14 +10,39 @@ export class SendAwaitComponent implements OnInit {
 
     stringToSend: string;
 
+    receivedData: Array<any>;
+
     ngOnInit() {
         this.waiting = false;
         this.stringToSend = '';
+
+        this.receivedData = new Array<any>();
+        this.receivedData.push(this.newReceivedData());
     }
 
     send() {
         this.waiting = true;
-        console.log(this.stringToSend);
+
+        // mocked
+        this.receivedData.unshift(this.newReceivedData());
+        this.waiting = false;
+    }
+
+    clearReceivedData() {
+        this.receivedData = new Array<any>();
+    }
+
+    private newReceivedData():any {
+        var currentData = {
+            id: Math.floor(Math.random() * 1e100),
+            dateTime: Date.now(),
+            originalString: this.stringToSend,
+            stringHash: this.stringToSend + 'hashed'
+        }
+
+        this.stringToSend = '';
+
+        return currentData;
     }
 
 }
